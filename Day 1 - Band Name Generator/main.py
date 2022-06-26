fc579927bacf8010688eb5fc3f364968a04e4a02
Sachin Kamath <kamathsachin@hotmail.com>
2022-06-25 23:56:00 -0700

    Completed Band Name Generator

--- Day 1 - Band Name Generator/main.py
+++ Day 1 - Band Name Generator/main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Welcome to the Band Name Generator.")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print(f"Your band name could be {city_name} {pet_name}")
