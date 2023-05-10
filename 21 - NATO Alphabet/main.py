178e9bb7ab0483e1efd6552a6a076049042fdab1
Sachin Kamath <kamathsachin@hotmail.com>
2023-05-09 22:25:46 -0700

    Updated to implement exception handling.

--- 21 - NATO Alphabet/main.py
+++ 21 - NATO Alphabet/main.py
@@ -4,6 +4,16 @@ phonetic_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data.iterrows()}
 
-user_input = input("Enter a word: ").upper()
-phonetic_list = [phonetic_dict[alpha] for alpha in user_input]
-print(phonetic_list)
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        phonetic_list = [phonetic_dict[alpha] for alpha in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
+
+
+generate_phonetic()
